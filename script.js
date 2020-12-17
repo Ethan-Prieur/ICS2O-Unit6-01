d876d4fa6985e706f3a97d948e558d67ef291d58
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2020-12-17 16:04:48 +0000

    fixed standard style issues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById("luck").addEventListener("click", myFunction);
-function myFunction() {
-  var x = Math.floor(Math.random() * 6) + 1;
-  var y = document.getElementById("box").value
-  y = parseInt(y);
-  if (x == y) {
-   alert("you guessed correctly!")
+document.getElementById('luck').addEventListener('click', myFunction)
+function myFunction( ) {
+  var x = Math.floor(Math.random() * 6) + 1
+  let y = document.getElementById('box').value
+  y = parseInt(y)
+  if (x === y) {
+    alert('you guessed correctly!')
   }
 }
