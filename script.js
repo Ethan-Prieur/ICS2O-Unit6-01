399fe84a1c3875df729c4b51e30b7491b12c6a83
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-04 03:21:34 +0000

    work please

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('luck').addEventListener('click', myFunction)
-function myFunction (){
-  let x = Math.floor(Math.random() * 6) + 1
+function myFunction () {
+  const x = Math.floor(Math.random() * 6) + 1
   let y = document.getElementById('box').value
   y = parseInt(y)
   if (x === y) {
